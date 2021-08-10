a2408197a14dc30f91eca94c6745e1652472a5fe
JesúsFranciscoLeyva <leyva527@gmail.com>
2021-08-10 08:27:04 -0600

    Initial Commit

---
+++ rutasCerro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rutasCerro
+//
+//  Created by Jesús Francisco Leyva Juárez on 10/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rutasCerro/rutasCerroApp.swift
@@ -0,0 +1,17 @@
+//
+//  rutasCerroApp.swift
+//  rutasCerro
+//
+//  Created by Jesús Francisco Leyva Juárez on 10/08/21.
+//
+
+import SwiftUI
+
+@main
+struct rutasCerroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
