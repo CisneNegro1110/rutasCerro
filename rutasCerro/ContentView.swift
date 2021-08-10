cf43bde32e40bff4f180ffe48fb635aa6feb42e0
JesúsFranciscoLeyva <leyva527@gmail.com>
2021-08-10 11:01:10 -0600

    Cerro de la memoria

--- rutasCerro/ContentView.swift
+++ rutasCerro/ContentView.swift
@@ -6,11 +6,20 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView()
+            Button(action: {
+                
+            }, label: {
+                Text("Show directions")
+                    
+            })
+            .padding(.bottom, 50)
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -19,3 +28,19 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct MapView: UIViewRepresentable {
+    typealias UIView = MKMapView
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 25.8087, longitude: -108.9689), span: MKCoordinateSpan(latitudeDelta: 0.012, longitudeDelta: 0.012))
+        mapView.setRegion(region, animated: true)
+        return mapView
+    }
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+    }
+}
+
